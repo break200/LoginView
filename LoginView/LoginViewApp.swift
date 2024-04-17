05a8eeb6f365fc7040538db838cc9ac8f72199c3
sanghee <sanghee.son@promr.co.kr>
2024-04-17 09:21:32 +0900

    Initial Commit

---
+++ LoginView/LoginViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewApp.swift
+//  LoginView
+//
+//  Created by break200 on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
